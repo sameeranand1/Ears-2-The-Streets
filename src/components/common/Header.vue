7f6dd4f0aa24e20babfd651a3f34925af5227c2c
Sameer Anand <sameer.anand@stonybrook.edu>
2016-08-06 02:27:24 -0400

    Update header

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -4,6 +4,18 @@
 
         <div class="Container">
 
+            <div class="Logo">
+                <img src="../../assets/images/logo.svg" alt="Ears 2 The Streets">
+            </div>
+
+            <div class="Navigation--left">
+
+            </div>
+
+            <div class="Navigation--right">
+
+            </div>
+
         </div>
 
     </div>
@@ -37,5 +49,27 @@
 
     .Site_Header .Container
         height 100%
+
+    .Site_Header .Logo
+        display inline-block
+        float left
+        height 100%
+
+    .Site_Header .Logo img
+        height 102px
+        margin-top 4px
+        width 300px
+
+    .Site_Header .Navigation--left
+        display inline-block
+        height 100%
+        margin-left 32px
+        width auto
+
+    .Site_Header .Navigation--right
+        display inline-block
+        float right
+        height 100%
+        width auto
 
 </style>
