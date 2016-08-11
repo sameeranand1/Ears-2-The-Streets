3084a84dfdae1c62bd4bc2432ebc1a0b58b4a1ca
Sameer Anand <sameer.anand@stonybrook.edu>
2016-08-11 02:14:20 -0400

    Update Home layout

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -60,14 +60,32 @@
         height 100%
 
     .Home .Column--left
-        lost-column 744/1000 0 20px
-
-    .Home .Column--right
-        lost-column 256/1000 0 20px
+        margin-bottom 20px
 
     .Home .Module
         min-height 300px
         lost-row 1 20px
+
+    @media (min-width 992px)
+
+        .Home .Column--left
+            --width: calc(1 - (300 / ( 992 - 30 )))
+            lost-column var(--width) 0 20px
+            margin-bottom 0
+
+        .Home .Column--right
+            --width: calc( 300 / ( 992 - 30 ))
+            lost-column var(--width) 0 20px
+
+    @media (min-width 1200px)
+
+        .Home .Column--left
+            --width: calc(1 - (300 / ( 1200 - 30 )))
+            lost-column var(--width) 0 20px
+
+        .Home .Column--right
+            --width: calc( 300 / ( 1200 - 30 ))
+            lost-column var(--width) 0 20px
 
 
 </style>
