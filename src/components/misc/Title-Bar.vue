8a3e5cf23d5e0e38de8a162cb3b7a60089421309
Sameer Anand <sameer.anand@stonybrook.edu>
2016-08-13 07:17:51 -0400

    Update homepage

--- src/components/misc/Title-Bar.vue
+++ src/components/misc/Title-Bar.vue
@@ -1,6 +1,20 @@
 <template>
-    {{ top }}
-    {{ bottom }}
+
+    <div class="Title_Bar">
+
+        <div class="Left">
+            <h4 class="Top">{{ top }}</h4>
+            <h4 class="Bottom">{{ bottom }}</h4>
+        </div>
+
+        <div class="Right">
+            <slot></slot>
+        </div>
+
+        <hr>
+
+    </div>
+
 </template>
 
 <script>
@@ -9,11 +23,83 @@
         props: 
         [ 
             'top', 
-            'bottom' 
+            'bottom'
         ]
     }
 </script>
 
 <style lang="stylus" scoped>
+
+    @require '../../stylus/common/colors'
+
+    .Title_Bar
+        background transparent
+        display flex
+        position relative
+        width 100%
+        z-index 3
+
+    .Title_Bar .Left,
+    .Title_Bar .Right
+        box-sizing border-box
+        display inline-block
+        padding 11px
+        position relative
+
+    .Title_Bar .Left
+        background repeating-linear-gradient(
+            -40deg,
+            #292a30,
+            #292a30 1px,
+            #313239 1px,
+            #313239 7px
+        )
+        width 220px
+        z-index 3
+
+    .Title_Bar .Left:after
+        border-bottom 5px solid $WHITE
+        border-left 4px solid transparent
+        border-right 10px solid $WHITE
+        bottom 3px
+        content ''
+        position absolute
+        right 0
+
+    .Title_Bar .Left:before
+        border-bottom 15px solid #A4AAAA
+        border-left 15px solid transparent
+        bottom 3px
+        content ''
+        position absolute
+        right 0
+
+    .Title_Bar .Right
+        flex 1
+
+    .Title_Bar .Top,
+    .Title_Bar .Bottom
+        font-family inherit
+        font-weight bold
+        text-transform uppercase
+
+    .Title_Bar .Top
+        color $BRAND_YELLOW
+        font-size 12px
+
+    .Title_Bar .Bottom
+        color $WHITE
+        font-size 20px
+
+    .Title_Bar hr
+        background none
+        border none
+        border-bottom 3px solid $BRAND_LIGHTER_BLUE
+        bottom 0
+        left 0
+        position absolute
+        right 0
+        width 100%
+        z-index 1
 
 </style>
--- src/components/mixtapes/Exclusive-Mixtape.vue
+++ src/components/mixtapes/Exclusive-Mixtape.vue
@@ -1,13 +1,39 @@
 <template>
-    <p>Exclusive Mixtape</p>
+    
+    <div class="Exclusive Mixtape">
+
+        <header class="Header">
+
+            <title-bar top="Exclusive" bottom="Mixtape"></title-bar>
+
+        </header>
+
+    </div>
+
 </template>
 
 <script>
 
-    export default {}
+    import TitleBar from '../misc/Title-Bar.vue'
+
+    export default 
+    {
+        components:
+        {
+            'title-bar': TitleBar
+        },
+
+        data()
+        {
+            return {
+            }
+        }
+    }
 
 </script>
 
 <style lang="stylus" scoped>
+
+    @require '../../stylus/common/colors'
 
 </style>
