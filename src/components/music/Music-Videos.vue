c792e97271d00d2f26e2599406b4ee4da068e949
Sameer Anand <sameer.anand@stonybrook.edu>
2016-08-26 00:18:14 -0400

    Update music videos page

--- src/components/music/Music-Videos.vue
+++ src/components/music/Music-Videos.vue
@@ -1,8 +1,34 @@
 <template>
 
-    <div class="Page">
+    <div class="Music_Videos Page">
 
-        <h2>Music Videos Page</h2>
+        <div class="Container">
+
+            <div class="Column--left">
+
+                <div class="Module"><exclusive-music-videos></exclusive-music-videos></div>
+
+            </div>
+
+            <div class="Column--right">
+
+                <button type="button" class="Button">Promote Your Music Video</button>
+                
+                <div class="Module">
+
+                    <h6 class="Featured_Event_Heading">Special E2TS Events / Promotion</h6>
+
+                    <featured-event></featured-event>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="Container">
+
+        </div>
 
     </div>
 
@@ -10,9 +36,16 @@
 
 <script>
 
+    import ExclusiveMusicVideos     from '../music/Exclusive-Music-Videos.vue'
+    import FeaturedEvent            from '../events/Featured-Event.vue'
+
     export default
     {
-
+        components:
+        {
+            'exclusive-music-videos': ExclusiveMusicVideos,
+            'featured-event': FeaturedEvent
+        }
     }
 
 </script>
@@ -21,14 +54,66 @@
 
     @require '../../stylus/common/colors'
 
-    .Page
-        align-items center
-        display flex
-        justify-content center
+    /*.Page
+        padding-top 0*/
 
-    .Page h2
-        color $BRAND_BLUE
-        font-size 15px
-        text-transform uppercase
+    .Music_Videos.Page .Container:first-child
+        lost-flex-container row
+        lost-utility clearfix
+        padding-top 20px
+
+    .Music_Videos.Page .Container:first-child .Column--left,
+    .Music_Videos.Page .Container:first-child .Column--right
+        height 100%
+        lost-utility clearfix
+
+    .Music_Videos.Page .Container:first-child .Column--left
+        lost-column var(--width) 0 20px flex
+        margin-bottom 20px
+        --width: 1
+
+    .Music_Videos.Page .Container:first-child .Column--right
+        lost-column var(--width) 0 20px flex
+        --width: 1
+
+    .Music_Videos.Page .Container:first-child .Column--right button
+        margin-bottom 20px
+        width 100%
+
+    .Music_Videos.Page .Container:first-child .Column--right .Featured_Event_Heading
+        color #898C9A
+        font-family inherit
+        font-size 13px
+        font-weight normal
+        margin-bottom 15px
+
+    .Music_Videos.Page .Container:first-child .Module
+        lost-waffle 1 1 20px flex
+        position relative
+
+    @media (min-width 768px)
+
+        .Music_Videos.Page .Container:first-child .Column--left
+            margin-bottom 0
+            --width: calc(1 - (300 / ( 768 - 30 )))
+
+        .Music_Videos.Page .Container:first-child .Column--right
+            --width: calc(300 / ( 768 - 30 ))
+    
+    @media (min-width 992px)
+
+        .Music_Videos.Page .Container:first-child .Column--left
+            --width: calc(1 - (300 / ( 992 - 30 )))
+
+        .Music_Videos.Page .Container:first-child .Column--right
+            --width: calc(300 / ( 992 - 30 ))
+    
+    @media (min-width 1200px)
+
+        .Music_Videos.Page .Container:first-child .Column--left
+            --width: calc(1 - (300 / ( 1200 - 30 )))
+
+        .Music_Videos.Page .Container:first-child .Column--right
+            --width: calc(300 / ( 1200 - 30 ))
 
 </style>
